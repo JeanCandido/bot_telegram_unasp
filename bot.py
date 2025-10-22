e67615dcd1c8f824dcc1216024a611736d3bbef8
Jean Candido <candidojean098@gmail.com>
2025-10-22 20:19:06 -0300

    Atualização do .env

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
-load_dotenv("./.env")
+load_dotenv(".env")
 updates = requests.get(f"https://api.telegram.org/bot{os.getenv('TELEGRAM_TOKEN')}/getUpdates")
 updates = updates.json()
 updates["result"][-1]
