357ca8e3c35899189e40485476194257071fafd1
Jean Candido <candidojean098@gmail.com>
2025-10-22 22:07:05 -0300

    Testando bot de calculadora

--- bot.py
+++ bot.py
@@ -1,7 +1,17 @@
 import requests
 import os
 from dotenv import load_dotenv
+
 load_dotenv(".env")
-updates = requests.get(f"https://api.telegram.org/bot{os.getenv('TELEGRAM_TOKEN')}/getUpdates")
-updates = updates.json()
-print(updates['result'][1])
+
+def updates():
+  updates = requests.get(f"https://api.telegram.org/bot{os.getenv('TELEGRAM_TOKEN')}/getUpdates?offset=-1")
+  updates = updates.json()
+  last_update = updates["result"][0]
+  if last_update:
+    last_date = last_update["message"]["date"]
+  return last_update["message"]["chat"]["id"], last_update["message"]["text"], last_date
+
+last_date = 0
+
+print(updates()[0])
