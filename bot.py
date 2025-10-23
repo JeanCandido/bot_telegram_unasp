4ffa60f4a8b156499417b1ec4f1c1e2f78e2fc6d
EA - Patríck Oliveira Pereira Guimaraes <patrick_guimaraes@educadventista.org>
2025-10-22 21:14:14 -0300

    mudo nada

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import requests
 import os
 from dotenv import load_dotenv
-load_dotenv("./.env")
+load_dotenv(".env")
 updates = requests.get(f"https://api.telegram.org/bot{os.getenv('TELEGRAM_TOKEN')}/getUpdates")
 updates = updates.json()
-updates["result"][-1]
+print(updates["result"][-1])
 
 #leu my friendo
